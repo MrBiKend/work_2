24704a7105b658dbb15b546e883f0e3522b9550b
BiKend <bikend.learn@gmail.com>
2024-05-18 10:39:48 +0400

    В этом обновлении добавлено поле name_ для хранения названия дороги и соответствующие методы для работы с ним.

--- Ex2.cpp
+++ Ex2.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
+#include <string>
 
 // Класс дороги
 class Road {
 private:
   int length_; // Длина дороги в метрах
   int lanes_; // Количество полос на дороге
+  std::string name_; // Название дороги
 
 public:
   // Конструктор без параметров
-  Road() : length_(0), lanes_(0) {}
+  Road() : length_(0), lanes_(0), name_("") {}
 
   // Конструктор с параметрами
-  Road(int length, int lanes) : length_(length), lanes_(lanes) {}
+  Road(int length, int lanes, const std::string& name) : length_(length), lanes_(lanes), name_(name) {}
 
   // Метод для установки значений полей
-  void setValues(int length, int lanes) {
+  void setValues(int length, int lanes, const std::string& name) {
     length_ = length;
     lanes_ = lanes;
+    name_ = name;
   }
 
   // Метод для получения длины дороги
@@ -28,11 +31,21 @@ public:
   int getLanes() const {
     return lanes_;
   }
+
+  // Метод для получения названия дороги
+  std::string getName() const {
+    return name_;
+  }
 };
 
 int main() {
   // Получаем данные о дороге
   int length, lanes;
+  std::string name;
+  std::cout << "Введите название дороги: ";
+  std::cin.ignore();
+  std::getline(std::cin, name);
+
   std::cout << "Введите длину дороги (в метрах): ";
   std::cin >> length;
 
@@ -40,10 +53,11 @@ int main() {
   std::cin >> lanes;
 
   // Создаем объект дороги
-  Road road(length, lanes);
+  Road road(length, lanes, name);
 
   // Выводим информацию о дороге
-  std::cout << "Длина дороги: " << road.getLength() << " метров" << std::endl;
+  std::cout << "Дорога \"" << road.getName() << "\":" << std::endl;
+  std::cout << "Длина: " << road.getLength() << " м" << std::endl;
   std::cout << "Количество полос: " << road.getLanes() << std::endl;
 
   return 0;
