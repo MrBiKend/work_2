30e0f8af9ba66aa4e411c5b75a93ebe0c9db1bc9
BiKend <bikend.learn@gmail.com>
2024-05-18 10:38:41 +0400

    Обновлен код с добавлением конструктора с параметрами, заменой метода для установки значений на setValues(), объявлением методов getLength() и getLanes() как константных.

--- Ex2.cpp
+++ Ex2.cpp
@@ -3,39 +3,44 @@
 // Класс дороги
 class Road {
 private:
-  int length; // Длина дороги в метрах
-  int lanes; // Количество полос на дороге
+  int length_; // Длина дороги в метрах
+  int lanes_; // Количество полос на дороге
 
 public:
   // Конструктор без параметров
-  Road() {}
+  Road() : length_(0), lanes_(0) {}
 
-  // Метод для установки значений полей с клавиатуры
-  void setValuesFromInput() {
-    std::cout << "Введите длину дороги (в метрах): ";
-    std::cin >> length;
+  // Конструктор с параметрами
+  Road(int length, int lanes) : length_(length), lanes_(lanes) {}
 
-    std::cout << "Введите количество полос: ";
-    std::cin >> lanes;
+  // Метод для установки значений полей
+  void setValues(int length, int lanes) {
+    length_ = length;
+    lanes_ = lanes;
   }
 
   // Метод для получения длины дороги
-  int getLength() {
-    return length;
+  int getLength() const {
+    return length_;
   }
 
   // Метод для получения количества полос на дороге
-  int getLanes() {
-    return lanes;
+  int getLanes() const {
+    return lanes_;
   }
 };
 
 int main() {
-  // Создаем объект дороги
-  Road road;
+  // Получаем данные о дороге
+  int length, lanes;
+  std::cout << "Введите длину дороги (в метрах): ";
+  std::cin >> length;
 
-  // Устанавливаем значения полей с клавиатуры
-  road.setValuesFromInput();
+  std::cout << "Введите количество полос: ";
+  std::cin >> lanes;
+
+  // Создаем объект дороги
+  Road road(length, lanes);
 
   // Выводим информацию о дороге
   std::cout << "Длина дороги: " << road.getLength() << " метров" << std::endl;
@@ -43,4 +48,3 @@ int main() {
 
   return 0;
 }
-
